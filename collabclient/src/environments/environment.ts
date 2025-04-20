c1ff4cf85fefb7663b4884b9b0346e1319db1ca5
08harshit <heyharshithere@gmail.com>
2025-04-20 12:11:01 +0530

    add env

---
+++ collabclient/src/environments/environment.ts
@@ -0,0 +1,10 @@
+export const environment = {
+  production: false,
+  spotify: {
+    clientId: '8dfee658cf304fe1b8212fac460b8cbe',
+    clientSecret: 'cd747c32ede149dfa0b8ddfe58ada4b1',
+    state: 'burger',
+    redirectUri: 'http://127.0.0.1:4200/callback',
+    scope: 'user-read-playback-state user-modify-playback-state user-read-currently-playing user-read-private user-read-email'
+  }
+};
