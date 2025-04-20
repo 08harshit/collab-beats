3f96899971776a051007f691a9ea0b8bb4ce42c4
08harshit <heyharshithere@gmail.com>
2025-04-20 10:22:53 +0530

    add connect-spotify auth empty component and route

---
+++ collabclient/src/app/spotify-oauth/spotify-oauth.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+
+@Component({
+  selector: 'app-spotify-oauth',
+  imports: [RouterOutlet],
+  templateUrl: './spotify-oauth.component.html',
+  styleUrl: './spotify-oauth.component.scss'
+})
+export class SpotifyOAuthComponent {
+
+}
