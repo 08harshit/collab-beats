668e60bed38197b2e8ea01c21b0505f920854387
08harshit <heyharshithere@gmail.com>
2025-04-26 11:53:09 +0530

    feat(callback.componenet):add req for addig user

--- collabclient/src/app/callback/callback.component.ts
+++ collabclient/src/app/callback/callback.component.ts
@@ -2,15 +2,20 @@ import { Component } from "@angular/core";
 import { Router } from "@angular/router";
 import { SpotifyService } from "../services/spotify.service";
 import { environment } from "../../environments/environment";
+import { HttpClient } from "@angular/common/http";
+import { catchError, switchMap } from "rxjs/operators";
+import { of } from "rxjs";
 
 @Component({
   selector: "app-callback",
   template: "<div>Processing login...</div>",
+  standalone: true
 })
 export class CallbackComponent {
   constructor(
     private spotifyService: SpotifyService,
-    private router: Router
+    private router: Router,
+    private httpClient: HttpClient
   ) {
     const urlParams = new URLSearchParams(window.location.search);
     const code = urlParams.get("code");
@@ -24,19 +29,30 @@ export class CallbackComponent {
     }
 
     if (code) {
-      this.spotifyService.exchangeToken(code).subscribe({
-        next: (response: any) => {
-          // Store tokens in localStorage or a state management solution
+      this.spotifyService.exchangeToken(code).pipe(
+        switchMap((response: any) => {
           localStorage.setItem('spotify_access_token', response.access_token);
           localStorage.setItem('spotify_refresh_token', response.refresh_token);
-          console.log('Tokens received:', response);
 
+          return this.httpClient.post('http://localhost:3000/users/add-user', {
+            access_token: response.access_token,
+            refresh_token: response.refresh_token,
+            expires_in: response.expires_in,
+          }).pipe(
+            catchError(error => {
+              console.error('Error saving user data:', error);
+              return of(null);
+            })
+          );
+        })
+      ).subscribe({
+        next: () => {
           this.router.navigate(['/']);
         },
         error: (error) => {
-          console.error('Token exchange error:', error);
+          console.error('Authentication error:', error);
           this.router.navigate(['/'], {
-            queryParams: { error: 'token_exchange_failed' }
+            queryParams: { error: 'authentication_failed' }
           });
         }
       });
